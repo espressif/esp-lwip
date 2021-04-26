b19311c59a98b8bb7ad75993a0b70fbe0f221826
Eric Koldeweij <eric@no-sense.net>
2021-04-26 10:28:36 +0200

    ip6: Fix crash in ip6_output with debug enabled
    It turns out the crash only occurs if LWIP_DEBUG is enabled. If the
    parameter dest is NULL the function tries to find a route using the
    destination address of the packet instead. If this fails as well a
    debug message is printed but it is still using dest causing a NULL
    pointer dereference and crash at src/core/ipv6/ip6.c line 1312.
    
    [erik@kryo.se: Apply fix to ip6_output_hinted as well]

--- src/core/ipv6/ip6.c
+++ src/core/ipv6/ip6.c
@@ -1315,6 +1315,7 @@ ip6_output(struct pbuf *p, const ip6_addr_t *src, const ip6_addr_t *dest,
     ip6_addr_copy_from_packed(src_addr, ip6hdr->src);
     ip6_addr_copy_from_packed(dest_addr, ip6hdr->dest);
     netif = ip6_route(&src_addr, &dest_addr);
+    dest = &dest_addr;
   }
 
   if (netif == NULL) {
@@ -1374,6 +1375,7 @@ ip6_output_hinted(struct pbuf *p, const ip6_addr_t *src, const ip6_addr_t *dest,
     ip6_addr_copy_from_packed(src_addr, ip6hdr->src);
     ip6_addr_copy_from_packed(dest_addr, ip6hdr->dest);
     netif = ip6_route(&src_addr, &dest_addr);
+    dest = &dest_addr;
   }
 
   if (netif == NULL) {
